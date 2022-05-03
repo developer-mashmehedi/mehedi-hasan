5eadf7c688b953f390cb63a64957e964a9751fcf
developer-mashmehedi <mehedihasan110577@gmail.com>
2022-05-03 12:24:42 -0700

    added experience

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Narail, Dhaka Bangladesh' // Title Location
     });
 }
